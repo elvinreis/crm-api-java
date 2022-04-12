000ce75cd06944c1cabb3bc7517bcef6846387b7
elvinreis <dosreiselvin@gmail.com>
2022-04-12 18:19:23 -0100

    Customer service created

---
+++ src/main/java/com/elvin/crmapi/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.elvin.crmapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository CustomerRepository;
+}
