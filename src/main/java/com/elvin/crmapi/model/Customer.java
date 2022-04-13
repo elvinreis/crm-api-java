3ac45d9ca9862dbfb8b5b2f4faaa9aff6689c763
elvinreis <dosreiselvin@gmail.com>
2022-04-13 19:03:42 -0100

    add strategy type annotation

--- src/main/java/com/elvin/crmapi/model/Customer.java
+++ src/main/java/com/elvin/crmapi/model/Customer.java
@@ -1,6 +1,8 @@
 package com.elvin.crmapi.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -10,6 +12,7 @@ import lombok.Data;
 public class Customer {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String surname;
