7598f2e884dac96e3ea0716b12fdbd1a054c5904
elvinreis <dosreiselvin@gmail.com>
2022-04-13 19:48:32 -0100

    fix issue with data order insertion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO customer VALUES(1,'Bruno','Vaz','vaz.bruno@gmail.com','2000/12/31');
-INSERT INTO customer VALUES(2,'Stevien','Brito','stevienbreet@gmail.com','1988/12/31');
+INSERT INTO customer(ID, NAME, SURNAME, EMAIL, BIRTHDATE) VALUES(1,'Bruno','Vaz','vaz.bruno@gmail.com','2000/12/31');
+INSERT INTO customer(ID, NAME, SURNAME, EMAIL, BIRTHDATE) VALUES(2,'Stevien','Brito','stevienbreet@gmail.com','1988/12/31');
